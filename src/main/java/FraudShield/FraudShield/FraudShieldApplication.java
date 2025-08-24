6f20b1e0c06f1341b57f4dd6a3b2dbedf583b313
Yi Zhang <y_zh426@outlook.com>
2025-08-24 22:27:47 +0800

    Initial commit

---
+++ src/main/java/FraudShield/FraudShield/FraudShieldApplication.java
@@ -0,0 +1,13 @@
+package FraudShield.FraudShield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FraudShield/FraudShield/FraudShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package FraudShield.FraudShield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
